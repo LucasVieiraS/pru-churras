06851adf7458f6c5917a09d5cbcb611f3585f6fb
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2023-02-16 14:50:17 -0300

    Added Timer / Points Counter

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  redScore: number = 0;
+  blueScore: number = 0;
+
   constructor() {}
 
+  increaseRedScore() {
+    this.redScore += 1;
+  }
+
+  increaseBlueScore() {
+    this.blueScore += 1;
+  }
 }
