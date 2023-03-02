737ad2dd6336ce14725d3335a395919326a67722
LucasVieiraS <lucas.silva3759@etec.sp.gov.br>
2023-03-01 23:25:08 -0300

    Added Heads or Tails

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,29 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  face: number = 1;
+  debounce: boolean = false;
+
   constructor() {}
+
+  randomNumber(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  flipCoin() {
+    if (this.debounce == true) return;
+    this.debounce = true;
+    const coinElement = document.getElementById('coin');
+    if (!coinElement) return;
+    coinElement.style.width = '0';
+    setTimeout(() => {
+      this.face = this.randomNumber(1, 3);
+      coinElement.setAttribute('src', this.face == 1 && '../../assets/cara.png' || '../../assets/coroa.png');
+      coinElement.style.width = '100%';
+      setTimeout(() => {
+        this.debounce = false;
+      }, 300);
+    }, 300);
+  }
 
 }
