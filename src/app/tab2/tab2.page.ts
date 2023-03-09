ce48e6073d09ea35948b4543078f11d0b19893c2
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2023-03-09 15:07:09 -0300

    Added Truco Function

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,10 +9,50 @@ export class Tab2Page {
 
   value: number = 1;
 
+  redScore: number = 0;
+  redMatches: number = 0;
+
+  blueScore: number = 0;
+  blueMatches: number = 0;
+
   constructor() {}
 
   increaseValueBy(element: any) {
-    console.log(element);
+    this.value += parseInt(element.target.innerHTML);
+  }
+
+  increaseRedTeamScore() {
+    this.redScore += this.value;
+    if (this.redScore < 12) return;
+    this.redMatches += 1;
+    this.redScore = 0;
+    this.blueScore = 0;
+  }
+
+  reduceRedTeamScore() {
+    this.redScore -= this.value;
+  }
+
+  increaseBlueTeamScore() {
+    this.blueScore += this.value;
+    if (this.blueScore < 12) return;
+    this.blueMatches += 1;
+    this.redScore = 0;
+    this.blueScore = 0;
+  }
+
+  reduceBlueTeamScore() {
+    this.blueScore -= this.value;
+  }
+
+  cleanup() {
+    this.value = 1;
+    /*
+    this.redScore = 0;
+    this.blueScore = 0;
+    this.redMatches = 0;
+    this.blueMatches = 0;
+    */
   }
 
 }
